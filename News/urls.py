c65f7715aa3f601dffec4d364c8437bf2abcf8aa
konmin123 <MindlinKS@yandex.ru>
2022-05-21 13:59:17 +0300

    add class News in News_Mod models.py

--- News/urls.py
+++ News/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', include('News_Api.urls'))
+    path('', include('News_Api.urls'))
 ]
