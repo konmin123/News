5e2eb727ac5dd168ecf5189e3920b76bdd95f388
konmin123 <MindlinKS@yandex.ru>
2022-05-22 11:46:50 +0300

    add for class NewsAdmin list_display_links and search_fields

--- News_Mod/admin.py
+++ News_Mod/admin.py
@@ -4,6 +4,8 @@ from .models import News
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
 
 
 admin.site.register(News, NewsAdmin)
