bf5273027ca05ce608331cca25d62b4ee72a70cc
konmin123 <MindlinKS@yandex.ru>
2022-05-23 11:54:31 +0300

    models.py add class Category

--- News_Mod/admin.py
+++ News_Mod/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import News
 
+
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
     list_display_links = ('id', 'title')
