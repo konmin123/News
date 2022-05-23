31a3cddb21e18063c5c2aa1de0b35ee2a9581c1c
konmin123 <MindlinKS@yandex.ru>
2022-05-23 12:15:03 +0300

    add Category on admin.py

--- News_Mod/admin.py
+++ News_Mod/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import News
+from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
@@ -9,5 +9,12 @@ class NewsAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
 
