418e5561211db585e80c69a78bb14d49b6da5da4
konmin123 <MindlinKS@yandex.ru>
2022-05-23 14:19:32 +0300

    changed the template

--- News_Mod/models.py
+++ News_Mod/models.py
@@ -8,7 +8,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Публичная новость')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    category = models.ForeignKey('Category', verbose_name='Категория', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -21,6 +21,9 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = 'Категория'
