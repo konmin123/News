b2443739cf12b186f5eb66637dd974ad2fb709ef
konmin123 <MindlinKS@yandex.ru>
2022-06-05 20:46:48 +0300

    change templates

--- News_Api/views.py
+++ News_Api/views.py
@@ -5,17 +5,14 @@ from News_Mod.models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': "Список новостей",
-        'categories': categories,
     }
     return render(request, 'News_Api/index.html', context=context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'News_Api/category.html', {'news': news, 'categories': categories, 'category': category})
+    return render(request, 'News_Api/category.html', {'news': news, 'category': category})
