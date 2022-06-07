03828e7a93217164dfb8913d269defed680c80e3
konmin123 <MindlinKS@yandex.ru>
2022-06-07 12:35:58 +0300

    refactor models.py class News (category)

--- News_Mod/models.py
+++ News_Mod/models.py
@@ -9,7 +9,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Публичная новость')
-    category = models.ForeignKey('Category', verbose_name='Категория', on_delete=models.PROTECT, null=True)
+    category = models.ForeignKey('Category', verbose_name='Категория', on_delete=models.PROTECT)
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={'news_id': self.pk})
